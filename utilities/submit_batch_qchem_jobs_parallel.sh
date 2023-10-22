8289b62989f2bb37374bae7ca002bf13b98dcca5
cthree-40 <clmalbon@gmail.com>
2023-10-22 15:43:48 -0400

    Added parallel jobs shell script.

---
+++ utilities/submit_batch_qchem_jobs_parallel.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -l
+#SBATCH --job-name=dev_qchem_job
+#SBATCH --out="slurm-%j.out"
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=24
+#SBATCH --mem=150GB
+
+# Set necessary environmental variables
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_SCHEDULE=dynamic
+#export OMP_PROC_BIND=true
+#export OMP_PLACES=threads
+
+# Set up development qchem environment
+source /home/clm96/.qcdevsetup
+
+# Threads for each qchem job process
+procthreads=`expr $OMP_NUM_THREADS / 2`
+export QCTHREADS="$procthreads"
+
+echo "Submitting BATCH dev-QCHEM job."
+echo "Current directory: $(pwd)"
+echo "OMP_NUM_THREADS = $OMP_NUM_THREADS"
+echo "OMP_SCHEDULE = $OMP_SCHEDULE"
+
+echo "QCTHREADS = $QCTHREADS"
+
+echo "Start directory: ${1}"
+echo "Final directory: ${2}"
+
+for i in $( seq ${1} ${2} ); do
+    cd $i
+    
+    if [ -e "hehhe/hehhe.input" ]; then
+        cd hehhe
+        cp hehhe.nbox_npts.txt nbox_npts.txt
+        cp hehhe.nbox_data.txt nbox_data.txt
+        # Run QChem
+        qchem -nt $OMP_NUM_THREADS hehhe.input > hehhe.output
+        cd ../
+    fi
+
+    if [ -e "hcn/hcn.input" ]; then
+        cd hcn
+        cp hcn.nbox_npts.txt nbox_npts.txt
+        cp hcn.nbox_data.txt nbox_data.txt
+        # Run QChem
+        qchem -nt $QCTHREADS hcn.input > hcn.output &
+    cd ../
+    fi
+
+    if [ -e "fhf/fhf.input" ]; then
+        cd fhf
+        cp fhf.nbox_npts.txt nbox_npts.txt
+        cp fhf.nbox_data.txt nbox_data.txt
+        # Run QChem
+        qchem -nt $QCTHREADS fhf.input > fhf.output &
+    cd ../
+    fi
+
+    wait
+    
+    cd ../
+done
